f06eb3512daf2ab79282c8de25d9173ea0f6c811
Adit Jain <ajleo899@gmail.com>
2019-12-04 14:51:34 -0600

    configuring project, nav bar added

---
+++ app/src/main/java/com/example/virtualcloset/MainActivity.java
@@ -0,0 +1,50 @@
+package Adit.Adams;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.virtualcloset.ClosetFragment;
+import com.example.virtualcloset.HomeFragment;
+import com.example.virtualcloset.ProfileFragment;
+import com.example.virtualcloset.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottom_nav = findViewById(R.id.bottom_navigation);
+        bottom_nav.setOnNavigationItemSelectedListener(navListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_closet:
+                            selectedFragment = new ClosetFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
